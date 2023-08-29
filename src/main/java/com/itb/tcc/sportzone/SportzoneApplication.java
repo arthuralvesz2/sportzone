1a11dfb79ee1db1bf5ba99edaedf9c58cf4164e3
arthuralvesz2 <arthur_alves.2007@hotmail.com>
2023-08-29 12:09:20 -0300

    Criação do ambiente de desenvolvimento

---
+++ src/main/java/com/itb/tcc/sportzone/SportzoneApplication.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.sportzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportzoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportzoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/tcc/sportzone/SportzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.sportzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
